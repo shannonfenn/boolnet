7d7713ba50cb79139c8e4d8e3e874e526fb5e5e3
Shannon Fenn <shannon.fenn@gmail.com>
2015-06-30 08:47:29 +1000

    moved RandomBooleanNetwork into boolnetwork.py

--- python/BoolNet/boolnetwork.py
+++ python/BoolNet/boolnetwork.py
@@ -200,3 +200,24 @@ class BoolNetwork:
 
     def clear_history(self):
         self._inverse_moves = []
+
+
+class RandomBoolNetwork(BoolNetwork):
+
+    def __init__(self, initial_gates, Ni, No, transfer_functions):
+        self._transfer_functions = np.array(
+            transfer_functions, dtype=np.uint8, copy=True)
+        super().__init__(initial_gates, Ni, No)
+
+    def _check_invariants(self):
+        super()._check_invariants()
+        if self._transfer_functions.shape != (self.Ng,):
+            raise ValueError('Invalid transfer function matrix shape: {}'.format(
+                self._transfer_functions.shape))
+
+    def __str__(self):
+        return super().__str__() + '\ntransfer functions:\n{}'.format(self._transfer_functions)
+
+    @property
+    def transfer_functions(self):
+        return self._transfer_functions
