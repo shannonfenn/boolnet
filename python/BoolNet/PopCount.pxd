54b31890e430e146ee707d41b657e435dcb9705a
Shannon Fenn <shannonfenn@gmail.com>
2015-07-01 10:47:05 +1000

    added floodcount methods to PopCount

--- python/BoolNet/PopCount.pxd
+++ python/BoolNet/PopCount.pxd
@@ -3,6 +3,7 @@ from Packing cimport packed_type_t
 cdef extern from "gmp.h":
     # unsigned long int mpn_popcount (mp_limb_t *s1p, mp_size_t n)
     unsigned long int mpn_popcount (packed_type_t *s1p, size_t n)
+    unsigned long int mpn_scan1 (packed_type_t *s1p, size_t bit)
 
     #void mpn_nand_n(mp_ptr rp, mp_srcptr s1p, mp_srcptr s2p, mp_size_t n)
     #void mpn_xor_n(mp_ptr rp, mp_srcptr s1p, mp_srcptr s2p, mp_size_t n)
@@ -10,3 +11,6 @@ cdef extern from "gmp.h":
 cpdef size_t popcount_matrix(packed_type_t[:, :] mat)
 cpdef size_t popcount_vector(packed_type_t[:] vec)
 cpdef size_t popcount_chunk(packed_type_t chunk)
+
+cpdef size_t floodcount_vector(packed_type_t[:] vec)
+cpdef size_t floodcount_chunk(packed_type_t chunk)
--- python/BoolNet/PopCount.pyx
+++ python/BoolNet/PopCount.pyx
@@ -1,25 +1,51 @@
-# cython: profile=False
 # distutils: libraries = gmp
+from Packing cimport PACKED_HIGH_BIT_SET, PACKED_SIZE
 
 # @cython.boundscheck(False)
 # @cython.wraparound(False)
 # @cython.nonecheck(False)
 # @cython.cdivision(True)
 cpdef size_t popcount_matrix(packed_type_t[:, :] mat):
-    cdef size_t n = mat.size
-    cdef packed_type_t* data_ptr = &mat[0, 0]
-    return mpn_popcount(data_ptr, n)
+    return mpn_popcount(&mat[0, 0], mat.size)
 
 
 cpdef size_t popcount_vector(packed_type_t[:] vec):
-    cdef size_t n = vec.size
-    cdef packed_type_t* data_ptr = &vec[0]
-    return mpn_popcount(data_ptr, n)
+    return mpn_popcount(&vec[0], vec.size)
 
 
 cpdef size_t popcount_chunk(packed_type_t chunk):
     return mpn_popcount(&chunk, 1)
-# # to get a pointer
-# cpdef test_pointer(data_type_t[:] view):
-#     cdef Py_ssize_t i, j, n = view.shape[0]
-#     cdef data_type_t * data_ptr = &view[0]
+
+
+cpdef size_t floodcount_vector(packed_type_t[:] vec):
+    cdef bint was_zero
+    cdef size_t pos
+
+    # to ensure there is at least a 1 at the end for mpn_scan1
+    # we need to add it, but also remember if the last bit was
+    # set so we can unset it, and also return the correct count
+    was_zero = ((vec[-1] & PACKED_HIGH_BIT_SET) == 0)
+    vec[-1] |= PACKED_HIGH_BIT_SET
+    pos = mpn_scan1(&vec[0], 0)
+    if was_zero:
+        vec[-1] &= ~PACKED_HIGH_BIT_SET
+        if pos == PACKED_SIZE - 1:
+            return 0
+    return pos + 1
+
+
+cpdef size_t floodcount_chunk(packed_type_t chunk):
+    cdef bint was_zero
+    cdef size_t pos
+
+    # to ensure there is at least a 1 at the end for mpn_scan1
+    # we need to add it, but also remember if the last bit was
+    # set so we can unset it, and also return the correct count
+    was_zero = ((chunk & PACKED_HIGH_BIT_SET) == 0)
+    chunk |= PACKED_HIGH_BIT_SET
+    pos = mpn_scan1(&chunk, 0)
+    if was_zero:
+        chunk &= ~PACKED_HIGH_BIT_SET
+        if pos == PACKED_SIZE - 1:
+            return 0
+    return pos + 1
