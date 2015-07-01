70f3059ed8c0f201492dafe9eb70eabc16b6aebc
Shannon Fenn <shannonfenn@gmail.com>
2015-07-01 23:40:07 +1000

    floodcount methods fixed
    - now take optional end mask size
    - now correctly return inverted count

--- python/BoolNet/bitcount.pxd
+++ python/BoolNet/bitcount.pxd
@@ -12,5 +12,5 @@ cpdef size_t popcount_matrix(packed_type_t[:, :] mat)
 cpdef size_t popcount_vector(packed_type_t[:] vec)
 cpdef size_t popcount_chunk(packed_type_t chunk)
 
-cpdef size_t floodcount_vector(packed_type_t[:] vec)
-cpdef size_t floodcount_chunk(packed_type_t chunk)
+cpdef size_t floodcount_vector(packed_type_t[:] vec, size_t end_mask_len=*)
+cpdef size_t floodcount_chunk(packed_type_t chunk, size_t end_mask_len=*)
--- python/BoolNet/bitcount.pyx
+++ python/BoolNet/bitcount.pyx
@@ -17,30 +17,31 @@ cpdef size_t popcount_chunk(packed_type_t chunk):
     return mpn_popcount(&chunk, 1)
 
 
-cpdef size_t floodcount_vector(packed_type_t[:] vec):
+cpdef size_t floodcount_vector(packed_type_t[:] vec, size_t end_mask_len=0):
     cdef bint was_zero
-    cdef size_t pos
+    cdef size_t pos, row_len = PACKED_SIZE * vec.size
 
-    # to ensure there is at least a 1 at the end for mpn_scan1
-    # we need to add it, but also remember if the last bit was
-    # set so we can unset it, and also return the correct count
+    # to ensure there is at least a 1 at the end for mpn_scan1 we need to add it, but also
+    # remember if the last bit was set so we can unset it, and also return the correct count
     was_zero = ((vec[-1] & PACKED_HIGH_BIT_SET) == 0)
     vec[-1] |= PACKED_HIGH_BIT_SET
     pos = mpn_scan1(&vec[0], 0)
     if was_zero:
         vec[-1] &= ~PACKED_HIGH_BIT_SET
-        if pos == PACKED_SIZE*vec.size - 1:
+        if pos == row_len - 1:
             return 0
-    return pos + 1
+    pos = row_len - pos
+    if pos <= end_mask_len:
+        return 0
+    return pos - end_mask_len
 
 
-cpdef size_t floodcount_chunk(packed_type_t chunk):
+cpdef size_t floodcount_chunk(packed_type_t chunk, size_t end_mask_len=0):
     cdef bint was_zero
     cdef size_t pos
 
-    # to ensure there is at least a 1 at the end for mpn_scan1
-    # we need to add it, but also remember if the last bit was
-    # set so we can unset it, and also return the correct count
+    # to ensure there is at least a 1 at the end for mpn_scan1 we need to add it, but also
+    # remember if the last bit was set so we can unset it, and also return the correct count
     was_zero = ((chunk & PACKED_HIGH_BIT_SET) == 0)
     chunk |= PACKED_HIGH_BIT_SET
     pos = mpn_scan1(&chunk, 0)
@@ -48,4 +49,7 @@ cpdef size_t floodcount_chunk(packed_type_t chunk):
         chunk &= ~PACKED_HIGH_BIT_SET
         if pos == PACKED_SIZE - 1:
             return 0
-    return pos + 1
+    pos = PACKED_SIZE - pos
+    if pos <= end_mask_len:
+        return 0
+    return pos - end_mask_len
