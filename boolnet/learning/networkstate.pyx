74e1ab073434bed8aebc6f1df2fee7d0c8f6d6fc
Shannon Fenn <shannonfenn@gmail.com>
2015-07-05 00:38:21 +1000

    chained network state added

--- boolnet/learning/networkstate.pyx
+++ boolnet/learning/networkstate.pyx
@@ -7,7 +7,9 @@ from copy import deepcopy
 from boolnet.network.boolnetwork import BoolNetwork
 from boolnet.bintools.biterror import STANDARD_EVALUATORS
 from boolnet.bintools.biterror cimport StandardEvaluator
-from boolnet.bintools.packing cimport packed_type_t, generate_end_mask, f_type, function_list
+from boolnet.bintools.biterror_chained import CHAINED_EVALUATORS
+from boolnet.bintools.biterror_chained cimport ChainedEvaluator
+from boolnet.bintools.packing cimport packed_type_t, generate_end_mask, f_type, function_list, PACKED_SIZE
 from boolnet.bintools.packing import packed_type
 
 
@@ -15,8 +17,7 @@ cdef class StaticNetworkState:
     cdef:
         readonly size_t Ne, Ni, No, Ng, cols
         readonly metric
-        packed_type_t[:, :] activation, inputs, outputs,
-        packed_type_t[:, :] target, error
+        packed_type_t[:, :] activation, inputs, outputs, target, error
         packed_type_t zero_mask
         StandardEvaluator err_evaluator
         public object network
@@ -242,7 +243,7 @@ cdef class StaticNetworkState:
             raise ValueError(('Network output # ({}) does not match target '
                              '({}).').format(network.No, self.No))
 
- cdef class ChainedNetworkState:
+cdef class ChainedNetworkState:
     cdef:
         readonly size_t Ne, Ni, No, Ng, cols
         readonly metric
@@ -252,7 +253,7 @@ cdef class StaticNetworkState:
         ChainedEvaluator err_evaluator
         public object network
 
-    def __init__(self, network, example_generator, window_size):
+    def __init__(self, network, example_generator, window_size, metric=None):
         ''' Sets up the activation and error matrices for a new network.
             Note: This copies the provided network, so do not expect modifications
                   to pass through transparently without reacquiring the new alias.'''
@@ -265,15 +266,15 @@ cdef class StaticNetworkState:
         self.cols = window_size
         self.block_width = (self.cols * PACKED_SIZE)
         if self.Ne % self.block_width == 0:
-           self.blocks = self.Ne //
+            self.blocks = self.Ne // self.block_width
+        else:
+            self.blocks = self.Ne // self.block_width + 1
 
         self.zero_mask = generate_end_mask(self.Ne)
         
         self.target = np.empty((self.No, self.cols), dtype=packed_type)
         # instantiate a matrix for activation
         self.activation = np.empty((network.Ng + self.Ni, self.cols), dtype=packed_type)
-        # copy inputs into activation matrix
-        self.activation[:self.Ni, :] = inputs
         # create input and output view into activation matrix
         self.inputs = self.activation[:self.Ni, :]
         self.outputs = self.activation[-<int>self.No:, :]
