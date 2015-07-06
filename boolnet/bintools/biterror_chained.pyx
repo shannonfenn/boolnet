cfc5886be9ca304fe49d67d8be43ca895e388de3
Shannon Fenn <shannon.fenn@gmail.com>
2015-07-06 11:59:59 +1000

    added performance flags to chained biterr module

--- boolnet/bintools/biterror_chained.pyx
+++ boolnet/bintools/biterror_chained.pyx
@@ -1,3 +1,5 @@
+# cython: language_level=3
+# cython: boundscheck=False, wraparound=False, nonecheck=False, cdivision=True, initializedcheck=False
 from boolnet.bintools.metric_names import (
     E1, E2L, E2M, E3L, E3M, E4L, E4M, E5L, E5M,
     E6L, E6M, E7L, E7M, ACCURACY, PER_OUTPUT, Metric)
