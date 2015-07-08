e6e4395628eac9863a8ee6d5c4838da8bd65062f
Shannon Fenn <shannon.fenn@gmail.com>
2015-07-09 09:57:59 +1000

    added defaults for data settings
    [data][No] defaults to [data][Nb]
    [data][window_size] to 4

--- boolnet/exptools/config_tools.py
+++ boolnet/exptools/config_tools.py
@@ -139,21 +139,26 @@ def file_instance(data_settings, sampling_settings):
     return pack_examples(inputs, targets, training_indices)
 
 
+def default_key(settings, key, default_value):
+    if key in settings:
+        return settings[key]
+    else:
+        return default_value
+
+
 def generated_instance(data_settings, sampling_settings):
     data_dir = data_settings['dir']
     Nb = data_settings['bits']
-    No = data_settings['No']
     op = operator_from_name(data_settings['operator'])
     N_operands = num_operands(op)
 
     Ni = (N_operands * Nb)
     N = 2**Ni
 
+    # by default the output width is the operand width
+    No = default_key(data_settings, 'No', Nb)
     # default window size of 4 (arbitrary at this point)
-    if 'window_size' in data_settings:
-        window_size = data_settings['window_size']
-    else:
-        window_size = 4
+    window_size = default_key(data_settings, 'window_size', 4)
 
     training_indices = load_samples(sampling_settings, data_dir, N, Ni)
     Ns, Ne = training_indices.shape
