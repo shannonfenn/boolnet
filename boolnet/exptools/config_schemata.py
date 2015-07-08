8ec9346c41675ebf3a5aa365f9cb374019648617
Shannon Fenn <shannonfenn@gmail.com>
2015-07-08 21:57:24 +1000

    updated config schemata with new changes

--- boolnet/exptools/config_schemata.py
+++ boolnet/exptools/config_schemata.py
@@ -2,9 +2,8 @@ import numpy as np
 from good import Schema, Required, In, All, Any, Range, Type, IsDir
 from good import message, Allow, Optional
 
-from boolnet.bintools.example_generator import PackedExampleGenerator
 from boolnet.bintools.metrics import all_metric_names
-from boolnet.exptools.boolmapping import BoolMapping
+from boolnet.exptools.boolmapping import FileBoolMapping, OperatorBoolMapping
 from boolnet.learning.learn_boolnet import LEARNERS, OPTIMISERS
 
 
@@ -130,8 +129,8 @@ config_schema = Schema({
     'configuration_number':     All(int, Range(min=0)),
     'training_set_number':      All(int, Range(min=0)),
     'inter_file_base':          str,
-    'training_mapping':         Any(Type(BoolMapping), Type(PackedExampleGenerator)),
-    'test_mapping':             Any(Type(BoolMapping), Type(PackedExampleGenerator)),
+    'training_mapping':         Any(Type(FileBoolMapping), Type(OperatorBoolMapping)),
+    'test_mapping':             Any(Type(FileBoolMapping), Type(OperatorBoolMapping)),
     'training_indices':         All(Type(np.ndarray), is_1d, is_int_arr),
     Optional('initial_gates'):  All(Type(np.ndarray), is_2d, is_int_arr),
     }, default_keys=Required)
