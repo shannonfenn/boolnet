2fd022c52d5aa9b692539766c49665db9f114f65
Shannon Fenn <shannonfenn@gmail.com>
2015-09-13 12:15:55 +1000

    setup script now builds all cython files

--- setup.py
+++ setup.py
@@ -10,13 +10,23 @@ extensions = [
               libraries=['gmp'],
               library_dirs=[os.path.expanduser('~/static/lib/')],
               include_dirs=[os.path.expanduser('~/static/include/')]),
+    Extension("boolnet.bintools.functions", ["boolnet/bintools/functions.pyx"]),
+    Extension("boolnet.bintools.biterror", ["boolnet/bintools/biterror.pyx"]),
+    Extension("boolnet.bintools.biterror_chained", ["boolnet/bintools/biterror_chained.pyx"]),
+    Extension("boolnet.bintools.operator_iterator", ["boolnet/bintools/operator_iterator.pyx"]),
+    Extension("boolnet.bintools.example_generator", ["boolnet/bintools/example_generator.pyx"]),
+
     Extension("boolnet.exptools.fastrand", ["boolnet/exptools/fastrand.pyx"],
               libraries=cython_gsl.get_libraries(),
               library_dirs=[cython_gsl.get_library_dir()],
               include_dirs=[cython_gsl.get_cython_include_dir()],),
+
     Extension("boolnet.network.algorithms", ["boolnet/network/algorithms.pyx"],
               language='c++'),
     Extension("boolnet.network.boolnetwork", ["boolnet/network/boolnetwork.pyx"],
+              language='c++'),
+
+    Extension("boolnet.learning.networkstate", ["boolnet/learning/networkstate.pyx"],
               language='c++'),
     ]
 
