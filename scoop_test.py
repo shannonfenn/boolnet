5f81f16b978b873320236ed3d4215f594cb08930
Shannon Fenn <shannon.fenn@gmail.com>
2015-11-30 14:11:30 +1100

    Full environment printing only on optional "verbose" flag.

--- scoop_test.py
+++ scoop_test.py
@@ -1,26 +1,32 @@
 import time
 import os
 import sys
+import argparse
 from scoop.futures import map_as_completed
 
 
-def print_details(idx):
+def print_details(verbose):
     # Check version
-    s = 'idx: {}'.format(idx)
-    s += '\nenviron: {}'.format(os.environ)
-    s += '\nHOSTNAME: {}'.format(os.environ.get("HOSTNAME"))
+    s = 'HOSTNAME: {}'.format(os.environ.get("HOSTNAME"))
     s += '\nCIBM_HOME: {}'.format(os.environ.get("CIBM_HOME"))
     s += '\npy ver: {}'.format(sys.version_info)
     s += '\npy loc: {}'.format(sys.executable)
+    if verbose:
+        s += '\nenviron: {}'.format(os.environ)
     time.sleep(3)
     return s
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--verbose', '-v', action='store_true')
+    args = parser.parse_args()
+
     # num_servers = 14 + 6
     num_servers = 3
     # uses unordered map to ensure results are dumped as soon as available
-    results = list(map_as_completed(print_details, range(num_servers + 1)))
+    results = list(map_as_completed(
+        print_details, [args.verbose]*(num_servers + 1)))
 
     for r in results:
         print(r)
