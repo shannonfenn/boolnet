cdd5c4c3d6552fb0297cafda28d1d944f5c1d165
Shannon Fenn <shannon.fenn@gmail.com>
2015-11-30 11:19:52 +1100

    Updated check_config.py to latest config generation flow.

--- check_config.py
+++ check_config.py
@@ -14,9 +14,9 @@ def main():
     parser.add_argument('file',
                         type=argparse.FileType('r'),
                         help='experiment config filename.')
-    parser.add_argument('--data-dir', default='experiments/datasets',
+    parser.add_argument('--data-dir', default='~/HMRI/experiments/datasets',
                         type=str, help='test data directory.')
-    parser.add_argument('--results-dir', default='experiments/results',
+    parser.add_argument('--result-dir', default='~/HMRI/experiments/results',
                         type=str, help='test result directory.')
 
     args = parser.parse_args()
@@ -24,14 +24,20 @@ def main():
     # load experiment file
     settings = yaml.load(args.file, Loader=yaml.CSafeLoader)
 
-    # MUST FIX THIS SINCE BASE_DIR will be code, not above
-    settings['data']['dir'] = os.path.abspath(args.data_dir)
+    data_dir = os.path.expanduser(args.data_dir)
+    result_dir = os.path.expanduser(args.result_dir)
 
-    settings['learner']['inter_file_base'] = os.path.join(args.results_dir,
-                                                          'temp', 'inter_')
+    # MUST FIX THIS SINCE BASE_DIR will be code, not above
+    settings['data']['dir'] = os.path.abspath(data_dir)
+
+    settings['learner']['inter_file_base'] = os.path.join(
+        result_dir, 'temp', 'inter_')
 
     # test generation of tasks
-    config_tools.generate_configurations(settings)
+    configurations = config_tools.generate_configurations(settings)
+    print('{} configurations.'.format(len(configurations)))
+    tasks = config_tools.generate_tasks(configurations)
+    print('{} tasks.'.format(len(tasks)))
 
 
 if __name__ == '__main__':
