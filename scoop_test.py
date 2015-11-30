a74f5911cec6ffaa0eda54a869c5f28aa2e0887c
Shannon Fenn <shannon.fenn@gmail.com>
2015-11-30 12:25:04 +1100

    scoop test script now returns strings instead of printing them in the function (for hopefully cleaner output)

--- scoop_test.py
+++ scoop_test.py
@@ -1,27 +1,28 @@
 import time
-import os                           # for mkdir
-import sys                          # for path, exit
-import scoop                        # for distributed parallellism
+import os
+import sys
+from scoop.futures import map_as_completed
 
 
 def print_details(idx):
     # Check version
-    print(idx)
-    print(os.environ.get("HOSTNAME"))
-    print(os.environ.get("CIBM_HOME"))
-    print(sys.version_info)
-    print(sys.executable)
+    s = 'idx: {}'.format(idx)
+    s += '\nHOSTNAME: {}'.format(os.environ.get("HOSTNAME"))
+    s += '\nCIBM_HOME: {}'.format(os.environ.get("CIBM_HOME"))
+    s += '\npy ver: {}'.format(sys.version_info)
+    s += '\npy loc: {}'.format(sys.executable)
     time.sleep(3)
-    return idx
+    return s
 
 
 def main():
     # num_servers = 14 + 6
     num_servers = 3
     # uses unordered map to ensure results are dumped as soon as available
-    for i in scoop.futures.map(print_details, range(num_servers + 1)):
-        pass
+    results = list(map_as_completed(print_details, range(num_servers + 1)))
 
+    for r in results:
+        print(r)
 
 if __name__ == '__main__':
     main()
