25a05505239770ab7db2ce0be663d1c38290027d
Shannon Fenn <shannon.fenn@gmail.com>
2015-11-30 21:46:12 +1100

    updated scoop_test.py output

--- scoop_test.py
+++ scoop_test.py
@@ -7,12 +7,14 @@ from scoop.futures import map_as_completed
 
 def print_details(verbose):
     # Check version
-    s = 'HOSTNAME: {}'.format(os.environ.get("HOSTNAME"))
-    s += '\nCIBM_HOME: {}'.format(os.environ.get("CIBM_HOME"))
+    s = '$HOST: {}'.format(os.environ.get("HOST"))
+    s = '\n$HOSTNAME: {}'.format(os.environ.get("HOSTNAME"))
+    s += '\n$CIBM_HOME: {}'.format(os.environ.get("CIBM_HOME"))
     s += '\npy ver: {}'.format(sys.version_info)
     s += '\npy loc: {}'.format(sys.executable)
     if verbose:
         s += '\nenviron: {}'.format(os.environ)
+    s += '\n'
     time.sleep(3)
     return s
 
@@ -22,14 +24,15 @@ def main():
     parser.add_argument('--verbose', '-v', action='store_true')
     args = parser.parse_args()
 
-    # num_servers = 14 + 6
-    num_servers = 3
+    num_servers = 14 + 6
+    # num_servers = 3
     # uses unordered map to ensure results are dumped as soon as available
     results = list(map_as_completed(
         print_details, [args.verbose]*(num_servers + 1)))
 
-    for r in results:
-        print(r)
+    with open('log.txt', 'w') as f:
+        for r in results:
+            f.write(r + '\n')
 
 if __name__ == '__main__':
     main()
