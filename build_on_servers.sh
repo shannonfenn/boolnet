067b4ce751b7f12fe5b921052e6e6c824bbbf22d
Shannon Fenn <shannon.fenn@gmail.com>
2015-12-01 15:50:46 +1100

    build_on_servers now builds locally and has a better cli

--- build_on_servers.sh
+++ build_on_servers.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+GRN='\033[0;32m'
+MAG='\033[0;35m'
+NC='\033[0m' # No Color
+
+echo -e "building ${GRN}locally${NC}"
+rm -f boolnet/*/*.so
+rm -f boolnet/*/*.c
+rm -f boolnet/*/*.cpp
+python setup.py build_ext --inplace > local_build.log 2>&1
+tail -n 1 local_build.log
+
+echo -e "building on ${GRN}shannon${NC}"
 {
 	ssh -T shannon << EOF
   source scoop_setup.sh
@@ -7,19 +19,20 @@
   python setup.py build_ext --inplace
 EOF
 } > shannon_build.log 2>&1
+tail -n 1 shannon_build.log
 
+echo -e "building on ${GRN}darwin15${NC}"
 {
 ssh -T darwin15 << EOF
   source scoop_setup.sh
   cd /home/cibm01/shannon/HMRI/code_dar/boolnet
   git clean -xdf
-  scl enable devtoolset-3 'python setup.py build_ext --inplace'
+  scl enable devtoolset-3 "python setup.py build_ext --inplace"
 EOF
 } > darwin15_build.log 2>&1
+tail -n 1 darwin15_build.log
 
+echo "distributing ${MAG}runexp.py${NC}"
 scp runexp.py shannon:HMRI/scripts/
 scp runexp.py darwin15:HMRI/scripts/
 cp -f runexp.py ~/HMRI/scripts/
-
-tail -n 1 shannon_build.log
-tail -n 1 darwin15_build.log
