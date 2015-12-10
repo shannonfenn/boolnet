5ea549a84bcfeae7253a55005247b3d1a5d04cdc
Shannon Fenn <shannon.fenn@gmail.com>
2015-12-10 14:21:51 +1100

    build_on_servers now uses more easily modified server names for build servers (one for UON and one for CIBM)

--- build_on_servers.sh
+++ build_on_servers.sh
@@ -3,6 +3,9 @@ GRN='\033[0;32m'
 MAG='\033[0;35m'
 NC='\033[0m' # No Color
 
+CIBM_SERVER=fisher
+UNI_SERVER=darwin15
+
 echo -e "building ${GRN}locally${NC}"
 rm -f boolnet/*/*.so
 rm -f boolnet/*/*.c
@@ -10,30 +13,30 @@ rm -f boolnet/*/*.cpp
 python setup.py build_ext --inplace > local_build.log 2>&1
 tail -n 1 local_build.log
 
-echo -e "building on ${GRN}shannon${NC}"
+echo -e "building on ${GRN}$CIBM_SERVER${NC}"
 {
-	ssh -T shannon << EOF
+	ssh -T $CIBM_SERVER << EOF
   source scoop_setup.sh
   cd /home/cibm01/shannon/HMRI/code_sf/boolnet
   git pull
   git clean -xdf
   python setup.py build_ext --inplace
 EOF
-} > shannon_build.log 2>&1
-tail -n 1 shannon_build.log
+} > cibm_server_build.log 2>&1
+tail -n 1 cibm_server_build.log
 
-echo -e "building on ${GRN}darwin15${NC}"
+echo -e "building on ${GRN}$UNI_SERVER${NC}"
 {
-ssh -T darwin15 << EOF
+ssh -T $UNI_SERVER << EOF
   source scoop_setup.sh
   cd /home/cibm01/shannon/HMRI/code_dar/boolnet
   git pull
   git clean -xdf
   scl enable devtoolset-3 "python setup.py build_ext --inplace"
 EOF
-} > darwin15_build.log 2>&1
-tail -n 1 darwin15_build.log
+} > uni_server_build.log 2>&1
+tail -n 1 uni_server_build.log
 
 echo "distributing ${MAG}runexp.py${NC}"
-scp runexp.py shannon:HMRI/scripts/
+scp runexp.py fisher:HMRI/scripts/
 cp -f runexp.py ~/HMRI/scripts/
