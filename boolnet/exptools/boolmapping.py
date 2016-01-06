e6fa1e1bdbc268b4a0ebdcdffa76f51bc7634dcb
Shannon Fenn <shannonfenn@gmail.com>
2016-01-06 22:40:25 +1100

    Bool mappings improved: both now allow generation of input and tgt matrices directly (note that doing so on a poor choice of Operator mapping may be expensive).

--- boolnet/exptools/boolmapping.py
+++ boolnet/exptools/boolmapping.py
@@ -1,19 +1,15 @@
 from boolnet.bintools.packing import pack_bool_matrix
-from collections import namedtuple
+from boolnet.bintools.example_generator import packed_from_operator
 
 
 class FileBoolMapping:
     def __init__(self, inputs, target, Ne):
-        self.inputs, self.target = self._validate(inputs, target, Ne)
-        self.Ne = Ne
-
-    @staticmethod
-    def _validate(inputs, target, Ne):
         if inputs.shape[0] != target.shape[0] != Ne:
-            raise ValueError(
-                'Dataset input (), target () and Ne () do not match.'.format(
-                    inputs.shape[0], target.shape[0], Ne))
-        return pack_bool_matrix(inputs), pack_bool_matrix(target)
+            raise ValueError('Input ({}), target ({}), Ne ({}) should match.'
+                             .format(inputs.shape[0], target.shape[0], Ne))
+        self.packed_inputs = pack_bool_matrix(inputs)
+        self.packed_target = pack_bool_matrix(target)
+        self.Ne = Ne
 
     @property
     def Ni(self):
@@ -24,12 +20,28 @@ class FileBoolMapping:
         return self.target.shape[0]
 
 
-OperatorBoolMapping = namedtuple(
-    'OperatorBoolMapping', [
-        'indices',
-        'Nb',
-        'Ni',
-        'No',
-        'window_size',
-        'operator',
-        'N'])
+class OperatorBoolMapping:
+    def __init__(self, indices, Nb, Ni, No, window_size, operator, N):
+        if Nb > Ni:
+            raise ValueError('Nb ({}) > Ni ({}).'.format(Nb, Ni))
+        self.Nb = Nb
+        self.Ni = Ni
+        self.No = No
+        self.window_size = window_size
+        self.operator = operator
+        self.N = N
+        self._packed_repr = None
+
+    @property
+    def packed_inputs(self):
+        if self.packed_repr is None:
+            self.packed_repr = packed_from_operator(
+                self.indices, self.Nb, self.No, self.operator, self.N)
+        return self.packed_repr[0]
+
+    @property
+    def packed_target(self):
+        if self.packed_repr is None:
+            self.packed_repr = packed_from_operator(
+                self.indices, self.Nb, self.No, self.operator, self.N)
+        return self.packed_repr[1]
