80a08b61382c93f82710ab5f2c4955ff8f53bdc5
Shannon Fenn <shannon.fenn@gmail.com>
2016-01-22 14:37:37 +1100

    fixed unfinished network reordering by adding inverse permutation method

--- boolnet/learning/learners.py
+++ boolnet/learning/learners.py
@@ -2,7 +2,7 @@ from copy import copy
 from collections import namedtuple
 import logging
 import numpy as np
-from boolnet.bintools.functions import PER_OUTPUT, function_from_name
+import boolnet.bintools.functions as gf
 from boolnet.bintools.packing import (
     unpack_bool_matrix, unpack_bool_vector, BitPackedMatrix)
 from boolnet.learning.networkstate import StandardBNState
@@ -10,25 +10,27 @@ import boolnet.learning.kfs as mfs
 
 
 LearnerResult = namedtuple('LearnerResult', [
-    'best_representations', 'best_errors', 'best_iterations',
+    'network', 'partial_networks', 'best_errors', 'best_iterations',
     'final_iterations', 'target_order', 'feature_sets', 'restarts'])
 
 
 def per_tgt_err_stop_criterion(bit):
-    return lambda ev, _: ev.function_value(PER_OUTPUT)[bit] <= 0
+    return lambda ev, _: ev.function_value(gf.PER_OUTPUT)[bit] <= 0
 
 
 def guiding_func_stop_criterion():
     return lambda _, error: error <= 0
 
 
+def inverse_permutation(permutation):
+    inverse = np.zeros_like(permutation)
+    for i, p in enumerate(permutation):
+        inverse[p] = i
+    return inverse
+
+
 class BasicLearner:
-
-    def _check_parameters(self, parameters):
-        pass
-
     def _setup(self, optimiser, parameters):
-        self._check_parameters(parameters)
         # Gate generation
         self.gate_generator = parameters['gate_generator']
         self.node_funcs = parameters['network']['node_funcs']
@@ -44,9 +46,15 @@ class BasicLearner:
         self.optimiser = optimiser
         self.opt_params = copy(parameters['optimiser'])
         gf_name = self.opt_params['guiding_function']
-        self.guiding_func_id = function_from_name(gf_name)
+        self.guiding_func_id = gf.function_from_name(gf_name)
         self.opt_params['guiding_function'] = (
             lambda x: x.function_value(self.guiding_func_id))
+
+        if self.guiding_func_id == gf.PER_OUTPUT:
+            raise ValueError('Invalid guiding function: \'PER_OUTPUT\'')
+        if max(self.node_funcs) > 15 or min(self.node_funcs) < 0:
+            raise ValueError('\'node_funcs\' must come from [0, 15]: {}'.
+                             format(self.node_funcs))
 
     def run(self, optimiser, parameters):
         self._setup(optimiser, parameters)
@@ -58,13 +66,15 @@ class BasicLearner:
 
         opt_result = self.optimiser.run(state, self.opt_params)
 
-        return LearnerResult(best_representations=[opt_result.representation],
-                             best_errors=[opt_result.error],
-                             best_iterations=[opt_result.best_iteration],
-                             final_iterations=[opt_result.iteration],
-                             target_order=None,
-                             feature_sets=None,
-                             restarts=[opt_result.restarts])
+        return LearnerResult(
+            network=opt_result.representation,
+            best_errors=[opt_result.error],
+            best_iterations=[opt_result.best_iteration],
+            final_iterations=[opt_result.iteration],
+            restarts=[opt_result.restarts],
+            target_order=None,
+            feature_sets=None,
+            partial_networks=[])
 
 
 class StratifiedLearner(BasicLearner):
@@ -75,7 +85,7 @@ class StratifiedLearner(BasicLearner):
         self.mfs_fname = parameters['inter_file_base']
         # Optional
         self.auto_target = parameters.get('auto_target', False)
-        self.use_minfs_selection = parameters.get('minfs', False)
+        self.use_minfs_selection = parameters.get('minfs_masking', False)
         self.fabcpp_opts = parameters.get('fabcpp_options', False)
         self.keep_files = parameters.get('keep_files', False)
         # Initialise
@@ -85,22 +95,12 @@ class StratifiedLearner(BasicLearner):
         self.feature_sets = np.empty((self.No, self.No),
                                      dtype=list)
 
-    def _check_parameters(self, parameters):
-        problem_funcs = ['PER_OUTPUT']
-        function_name = parameters['optimiser']['guiding_function']
-        if function_name in problem_funcs:
-            raise ValueError('Invalid guiding function: {}.'
-                             .format(function_name))
-        if max(self.node_funcs) > 15 or min(self.node_funcs) < 0:
-            raise ValueError('\'node_funcs\' must come from [0, 15]: {}'.
-                             format(self.node_funcs))
-
     def _determine_next_target(self, strata, inputs):
         all_targets = set(range(self.No))
         not_learned = list(all_targets.difference(self.learned_targets))
         if self.auto_target:
             # find minFS for all unlearned targets
-            self._record_feature_sets(inputs, not_learned)
+            self._record_feature_sets(strata, inputs, not_learned)
             feature_sets_sizes = [len(l) for l in
                                   self.feature_sets[strata][not_learned]]
             indirect_simplest_target = np.argmin(feature_sets_sizes)
@@ -108,16 +108,15 @@ class StratifiedLearner(BasicLearner):
         elif self.use_minfs_selection:
             target = min(not_learned)
             # find minFS for just the next target
-            self._record_feature_sets(inputs, [target])
+            self._record_feature_sets(strata, inputs, [target])
             return target
         else:
             return min(not_learned)
 
-    def _record_feature_sets(self, inputs, targets):
+    def _record_feature_sets(self, strata, inputs, targets):
         # keep a log of the feature sets found at each iteration
-        strata = len(self.learned_targets)
         for t in targets:
-            fs = self._get_fs(inputs, t)
+            fs = self._get_fs(inputs, t, strata)
             self.feature_sets[strata, t] = fs
 
     def _get_fs(self, inputs, target_index, strata):
@@ -202,14 +201,18 @@ class StratifiedLearner(BasicLearner):
 
         return StandardBNState(accumulated_gates, new_problem_matrix)
 
-    def reorder_network(self, state):
+    def reorder_network(self, network):
         # all non-output gates are left alone, and the output gates are
         # reordered by the inverse permutation of "learned_targets"
+        print(network.Ng, network.No)
+        Ng, No = network.Ng, network.No
         new_gate_order = np.concatenate((
-            np.arange(state.Ng - state.No),
-            np.argsort(self.learned_targets).tolist()))
-        new_gates = state.gates[new_gate_order]
-        return StandardBNState(new_gates, self.problem_matrix)
+            np.arange(Ng - No),
+            inverse_permutation(self.learned_targets) + Ng - No))
+        new_gates = np.array(network.gates)
+        print(new_gate_order)
+        new_gates = new_gates[new_gate_order]
+        network.set_gates(new_gates)
 
     def run(self, optimiser, parameters):
         # setup accumulated network
@@ -233,11 +236,12 @@ class StratifiedLearner(BasicLearner):
             target = self._determine_next_target(i, inputs)
 
             partial_problem = self.make_partial_problem(i, target, inputs)
+
             gates = self.next_gates(i, partial_problem)
             state = StandardBNState(gates, partial_problem)
 
-            # generate an end condition based on the current target
-            criterion = guiding_func_stop_criterion(target)
+            # generate an end condition based on the error
+            criterion = guiding_func_stop_criterion()
             self.opt_params['stopping_criterion'] = criterion
 
             # optimise
@@ -247,23 +251,25 @@ class StratifiedLearner(BasicLearner):
 
             # build up final network by inserting this partial result
             result_state = StandardBNState(
-                partial_result.representation, partial_problem)
+                partial_result.representation.gates, partial_problem)
             accumulated_network = self.join_networks(
                 accumulated_network, result_state, i, target)
 
             # don't include output gates as possible inputs
             # new Ni = Ng - No + Ni
-            inputs = accumulated_network.activation_matrix[:-i, :]
+            No = accumulated_network.No
+            inputs = accumulated_network.activation_matrix[:-No, :]
             inputs = BitPackedMatrix(inputs, Ne=self.Ne, Ni=inputs.shape[0])
 
             self.learned_targets.append(target)
 
         # reorder the outputs to match the supplied target order
         # NOTE: This is why output gates are not included as possible inputs
-        opt_results[-1].state = self.reorder_network(opt_results[-1].state)
+        self.reorder_network(accumulated_network.representation)
 
         return LearnerResult(
-            best_representations=[r.state.gates for r in opt_results],
+            network=accumulated_network.representation,
+            partial_networks=[r.representation for r in opt_results],
             best_errors=[r.error for r in opt_results],
             best_iterations=[r.best_iteration for r in opt_results],
             final_iterations=[r.iteration for r in opt_results],
