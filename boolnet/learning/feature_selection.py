cd2128b18c9b19bc9638385e95358db6bb8a6bec
Shannon Fenn <shannonfenn@gmail.com>
2016-04-18 22:53:04 +1000

    more feature selection methods: max-entropy, max-feature-diversity and max-pattern-diversity

--- boolnet/learning/feature_selection.py
+++ boolnet/learning/feature_selection.py
@@ -1,12 +1,108 @@
 import numpy as np
-from subprocess import check_call
-import os
-import re
+from minizinc_tools import mzn_all_minfs
+from itertools import combinations
+from collections import defaultdict
+from scipy.stats import entropy
+
+
+def diversity(patterns):
+    total = 0
+    for p0, p1 in combinations(patterns, 2):
+        total += np.sum(x != y for x, y in zip(p0, p1))
+    return total
+
+
+def feature_diversity(all_features, fs_indices):
+    return diversity(all_features[:, fs_indices].T)
+
+
+def pattern_diversity(all_features, fs_indices):
+    return diversity(all_features[:, fs_indices])
+
+
+def feature_set_entropy(all_features, fs_indices):
+    fs = all_features[:, fs_indices]
+    counts = defaultdict(int)
+    for pattern in fs:
+        counts[tuple(pattern)] += 1
+    return entropy(counts.values(), base=2)
+
+
+def build_coverage(features, target):
+    Ne, Nf = features.shape
+    class_0_indices = np.where(target == 0)
+    class_1_indices = np.where(target)
+    num_eg_pairs = class_0_indices.size * class_1_indices.size
+    coverage_matrix = np.zeros((num_eg_pairs, Nf), dtype=np.uint8)
+    i = 0
+    for i0 in class_0_indices:
+        for i1 in class_1_indices:
+            for f in range(Nf):
+                if features[i0, f] != features[i1, f]:
+                    coverage_matrix[i, f] = 1
+            i += 1
+    return coverage_matrix
+
+
+def all_minfs(features, target):
+    coverage_matrix = build_coverage(features, target)
+    feature_sets = mzn_all_minfs(coverage_matrix)
+
+
+def best_feature_set(features, target, method):
+    ''' Takes a featureset matrix and target vector and finds a minimum FS.
+    features    - <2D numpy array> in example x feature format.
+    target      - <1D numpy array> of the same number of rows as features
+    method      - <string> which method to use to pick best feature set.
+    returns     - <1D numpy array> feature indices representing best FS
+                  according to given method.'''
+    if method == 'card':
+        feature_sets = all_minfs(features, target)
+        rand_index = np.random.randint(len(feature_sets))
+        return feature_sets[rand_index, :]
+    elif method == 'card>ent':
+        feature_sets = all_minfs(features, target)
+        entropies = [feature_set_entropy(features, fs) for fs in feature_sets]
+        best_fs = numpy.argmax(entropies)
+        return feature_sets[best_fs]
+    elif method == 'card>fdiv':
+        feature_sets = all_minfs(features, target)
+        feature_diversities = [feature_diversity(features, fs)
+                               for fs in feature_sets]
+        best_fs = numpy.argmax(feature_diversities)
+        return feature_sets[best_fs]
+    elif method == 'card>pdiv':
+        feature_sets = all_minfs(features, target)
+        pattern_diversities = [pattern_diversity(features, fs)
+                               for fs in feature_sets]
+        best_fs = numpy.argmax(pattern_diversities)
+        return feature_sets[best_fs]
+    else:
+        raise ValueError('Invalid method for feature selection: {}'.format(
+            method))
+
+
+def multi_target_minimum_feature_sets(features, targets, file_name_base,
+                                      options, keep_files):
+    ''' Takes a featureset matrix and target vector and finds a minimum FS.
+    features    - 2D numpy array-like in example x feature format.
+    target      - 2D numpy array-like of the same number of rows as features
+    returns     - list of 1D numpy array-likes of feature indices representing
+                  a feature set of minimal cardinality for each target.'''
+    num_targets = targets.shape[1]
+    feature_sets = [
+        minimum_feature_set(features, targets[:, t], file_name_base,
+                            options, keep_files)
+        for t in range(num_targets)]
+    return feature_sets
+
+
+# import os
+# import re
 
 # regex for matching feature lines
-FEATURE_RE = re.compile('[0-9]+')
+# FEATURE_RE = re.compile('[0-9]+')
 
-# from itertools import combinations
 # def is_feature_set(fs, target):
 #     # generator for the example index pairs to check
 #     to_check = ((i1, i2) for i1, i2 in combinations(range(len(target)), 2)
@@ -17,94 +113,65 @@ FEATURE_RE = re.compile('[0-9]+')
 #     return not any(np.array_equal(fs[e1], fs[e2]) for e1, e2 in to_check)
 
 
-def abk_file(features, target, file_name):
-    n_examples, n_features = features.shape
+# def abk_file(features, target, file_name):
+#     n_examples, n_features = features.shape
 
-    feature_numbers = np.reshape(np.arange(n_features), (n_features, 1))
+#     feature_numbers = np.reshape(np.arange(n_features), (n_features, 1))
 
-    abk_data = np.hstack((feature_numbers, features.T))
+#     abk_data = np.hstack((feature_numbers, features.T))
 
-    sample_names = '\t'.join('s' + str(i) for i in range(n_examples))
+#     sample_names = '\t'.join('s' + str(i) for i in range(n_examples))
 
-    header = 'FEATURESINROWS\nTARGETPRESENT\nLAST\n{}\n{}\ndummy\t{}'.format(
-        n_features, n_examples, sample_names)
+#     header = 'FEATURESINROWS\nTARGETPRESENT\nLAST\n{}\n{}\ndummy\t{}'.format(
+#         n_features, n_examples, sample_names)
 
-    target_row = '\t' + '\t'.join(str(x) for x in target) + '\n'
+#     target_row = '\t' + '\t'.join(str(x) for x in target) + '\n'
 
-    np.savetxt(file_name, abk_data, fmt='%d', delimiter='\t',
-               header=header, footer=target_row, comments='')
-
-
-def FABCPP_cmd_line(features, target, file_name_base, options, keep_files):
-    abk_file_name = file_name_base + '.abk'
-    log_file_name = file_name_base + '.log'
-    err_file_name = file_name_base + '.err'
-    out_file_name = file_name_base + '.sol'
-
-    # check if files exist and raise exception
-
-    # write abk file
-    abk_file(features, target, abk_file_name)
-
-    # ensure options is a mapping type if another 'False' type is given
-    if not options:
-        options = {}
-
-    model = options.get('model', 6)
-    alpha = options.get('a_min', 1)
-    beta = options.get('b_min', 0)
-
-    cmd_string = ['fabcpp', '-i', abk_file_name, '-o', file_name_base,
-                  '-m', str(model), '-A', str(alpha), '-B', str(beta),
-                  '-O', 'max:feature_degree']
-
-    if model == 1:
-        cover = options.get('cover', 'alfa')
-        cmd_string += ['-y', cover]
-
-    # run fabcpp
-    with open(log_file_name, 'w') as log, open(err_file_name, 'w') as err:
-        check_call(cmd_string, stdout=log, stderr=err)
-
-    # parse output
-    with open(out_file_name) as out:
-        # pull lines from outfile which match the feature name regex
-        k_feature_set = [int(l.strip()) for l in out
-                         if FEATURE_RE.match(l.strip())]
-
-    if not keep_files:
-        os.remove(abk_file_name)
-        os.remove(log_file_name)
-        os.remove(err_file_name)
-        os.remove(out_file_name)
-
-    return np.array(k_feature_set)
-
-    # TODO: Use model 5 to compute other FSs
+#     np.savetxt(file_name, abk_data, fmt='%d', delimiter='\t',
+#                header=header, footer=target_row, comments='')
 
 
-def minimum_feature_set(features, target, file_name_base,
-                        fabcpp_options, keep_files):
-    ''' Takes a featureset matrix and target vector and finds a minimum FS.
-    featureset  - assumed to be a 2D numpy array in example x feature format.
-    target      - assumed to be a 1D numpy array of the same number of rows
-                  as featureset
-    returns     - a 1D numpy array of feature indices representing a FS
-                  of minimal cardinality.'''
+# def FABCPP_cmd_line(features, target, file_name_base, options, keep_files):
+#     abk_file_name = file_name_base + '.abk'
+#     log_file_name = file_name_base + '.log'
+#     err_file_name = file_name_base + '.err'
+#     out_file_name = file_name_base + '.sol'
 
-    return FABCPP_cmd_line(features, target, file_name_base,
-                           fabcpp_options, keep_files)
+#     # check if files exist and raise exception
 
+#     # write abk file
+#     abk_file(features, target, abk_file_name)
 
-def multi_target_minimum_feature_sets(features, targets, file_name_base,
-                                      fabcpp_options, keep_files):
-    ''' Takes a featureset matrix and target vector and finds a minimum FS.
-    features    - 2D numpy array-like in example x feature format.
-    target      - 2D numpy array-like of the same number of rows as features
-    returns     - list of 1D numpy array-likes of feature indices representing
-                  a feature set of minimal cardinality for each target.'''
-    num_targets = targets.shape[1]
-    feature_sets = [FABCPP_cmd_line(features, targets[:, t], file_name_base,
-                                    fabcpp_options, keep_files)
-                    for t in range(num_targets)]
-    return feature_sets
+#     # ensure options is a mapping type if another 'False' type is given
+#     if not options:
+#         options = {}
+
+#     model = options.get('model', 6)
+#     alpha = options.get('a_min', 1)
+#     beta = options.get('b_min', 0)
+
+#     cmd_string = ['fabcpp', '-i', abk_file_name, '-o', file_name_base,
+#                   '-m', str(model), '-A', str(alpha), '-B', str(beta),
+#                   '-O', 'max:feature_degree']
+
+#     if model == 1:
+#         cover = options.get('cover', 'alfa')
+#         cmd_string += ['-y', cover]
+
+#     # run fabcpp
+#     with open(log_file_name, 'w') as log, open(err_file_name, 'w') as err:
+#         check_call(cmd_string, stdout=log, stderr=err)
+
+#     # parse output
+#     with open(out_file_name) as out:
+#         # pull lines from outfile which match the feature name regex
+#         k_feature_set = [int(l.strip()) for l in out
+#                          if FEATURE_RE.match(l.strip())]
+
+#     if not keep_files:
+#         os.remove(abk_file_name)
+#         os.remove(log_file_name)
+#         os.remove(err_file_name)
+#         os.remove(out_file_name)
+
+#     return np.array(k_feature_set)
