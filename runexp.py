822029ff6beab9d9e94723d1a3ee4ea8a8478049
Shannon Fenn <shannon.fenn@gmail.com>
2016-05-30 12:40:00 +1000

    Fixed incorrect key for debug level
    was requesting 'logging_level' instead of 'debug_level' resulting in permanent state of 'none'

--- runexp.py
+++ runexp.py
@@ -23,7 +23,7 @@ def initialise_logging(settings, result_dir):
                   'debug': logging.DEBUG,
                   'none': logging.ERROR}
     # default to 'none'
-    log_level = level_dict[settings.get('logging_level', 'none')]
+    log_level = level_dict[settings.get('debug_level', 'none')]
     logging.basicConfig(filename=os.path.join(result_dir, 'log'),
                         level=log_level)
 
