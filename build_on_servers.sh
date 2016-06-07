df61ba76e1845a953a95a2b2654faa34cb1f76ce
Shannon Fenn <shannon.fenn@gmail.com>
2016-06-07 16:07:34 +1000

    Better cleaning in setup.py

--- build_on_servers.sh
+++ build_on_servers.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 GRN='\033[0;32m'
 MAG='\033[0;35m'
 NC='\033[0m' # No Color
@@ -6,37 +6,37 @@ NC='\033[0m' # No Color
 CIBM_SERVER=fisher
 UNI_SERVER=darwin15
 
-echo -e "building ${GRN}locally${NC}"
-rm -f boolnet/*/*.so
-rm -f boolnet/*/*.c
-rm -f boolnet/*/*.cpp
-python setup.py build_ext --inplace > local_build.log 2>&1
+printf "building ${GRN}locally${NC}\n"
+python setup.py clean > local_build.log 2>&1
+python setup.py build_ext --inplace >> local_build.log 2>&1
 tail -n 1 local_build.log
 
-echo -e "building on ${GRN}$CIBM_SERVER${NC}"
+printf "building on ${GRN}$CIBM_SERVER${NC}\n"
 {
 	ssh -T $CIBM_SERVER << EOF
   source scoop_setup.sh
   cd /home/cibm01/shannon/HMRI/code_sf/boolnet
   git pull
-  git clean -xdf
+  python setup.py clean
   python setup.py build_ext --inplace
+  pip install -e .
 EOF
 } > cibm_server_build.log 2>&1
 tail -n 1 cibm_server_build.log
 
-echo -e "building on ${GRN}$UNI_SERVER${NC}"
+printf "building on ${GRN}$UNI_SERVER${NC}\n"
 {
 ssh -T $UNI_SERVER << EOF
   source scoop_setup.sh
   cd /home/cibm01/shannon/HMRI/code_dar/boolnet
   git pull
-  git clean -xdf
+  python setup.py clean
   scl enable devtoolset-3 "python setup.py build_ext --inplace"
+  pip install -e .
 EOF
 } > uni_server_build.log 2>&1
 tail -n 1 uni_server_build.log
 
-echo "distributing ${MAG}runexp.py${NC}"
+printf "distributing ${MAG}runexp.py${NC}\n"
 scp runexp.py fisher:HMRI/scripts/
 cp -f runexp.py ~/HMRI/scripts/
--- setup.py
+++ setup.py
@@ -5,52 +5,84 @@ except ImportError:
 from Cython.Distutils import Extension, build_ext
 import numpy as np
 import cython_gsl
+import sys
+import glob
+import os
 
 include_dirs = [np.get_include(),
                 cython_gsl.get_cython_include_dir()]
 
+args = sys.argv[1:]
+
+# get rid of intermediate and library files
+if "clean" in args:
+    print("Deleting cython files...")
+    to_remove = []
+    # C
+    to_remove += glob.glob('boolnet/bintools/packing.c')
+    to_remove += glob.glob('boolnet/bintools/bitcount.c')
+    to_remove += glob.glob('boolnet/bintools/functions.c')
+    to_remove += glob.glob('boolnet/bintools/biterror.c')
+    to_remove += glob.glob('boolnet/bintools/biterror_chained.c')
+    to_remove += glob.glob('boolnet/bintools/operator_iterator.c')
+    to_remove += glob.glob('boolnet/bintools/example_generator.c')
+    to_remove += glob.glob('boolnet/exptools/fastrand.c')
+    # C++
+    to_remove += glob.glob('boolnet/network/algorithms.cpp')
+    to_remove += glob.glob('boolnet/network/boolnet.cpp')
+    to_remove += glob.glob('boolnet/learning/networkstate.cpp')
+    # Static lib files
+    to_remove += glob.glob('boolnet/bintools/*.so')
+    to_remove += glob.glob('boolnet/exptools/*.so')
+    to_remove += glob.glob('boolnet/network/*.so')
+    to_remove += glob.glob('boolnet/learning/*.so')
+    for f in to_remove:
+        os.remove(f)
+
+
+# We want to always use build_ext --inplace
+if args.count('build_ext') > 0 and args.count('--inplace') == 0:
+    sys.argv.insert(sys.argv.index('build_ext')+1, '--inplace')
 
 extensions = [
-    Extension("boolnet.bintools.packing",
-              ["boolnet/bintools/packing.pyx"],
+    Extension('boolnet.bintools.packing',
+              ['boolnet/bintools/packing.pyx'],
               include_dirs=include_dirs),
-    Extension("boolnet.bintools.bitcount",
-              ["boolnet/bintools/bitcount.pyx"],
+    Extension('boolnet.bintools.bitcount',
+              ['boolnet/bintools/bitcount.pyx'],
               libraries=['gmp'],
               include_dirs=include_dirs),
-    Extension("boolnet.bintools.functions",
-              ["boolnet/bintools/functions.pyx"],
+    Extension('boolnet.bintools.functions',
+              ['boolnet/bintools/functions.pyx'],
               include_dirs=include_dirs),
-    Extension("boolnet.bintools.biterror",
-              ["boolnet/bintools/biterror.pyx"],
+    Extension('boolnet.bintools.biterror',
+              ['boolnet/bintools/biterror.pyx'],
               include_dirs=include_dirs),
-    Extension("boolnet.bintools.biterror_chained",
-              ["boolnet/bintools/biterror_chained.pyx"],
+    Extension('boolnet.bintools.biterror_chained',
+              ['boolnet/bintools/biterror_chained.pyx'],
               include_dirs=include_dirs),
-    Extension("boolnet.bintools.operator_iterator",
-              ["boolnet/bintools/operator_iterator.pyx"],
+    Extension('boolnet.bintools.operator_iterator',
+              ['boolnet/bintools/operator_iterator.pyx'],
               include_dirs=include_dirs),
-    Extension("boolnet.bintools.example_generator",
-              ["boolnet/bintools/example_generator.pyx"],
+    Extension('boolnet.bintools.example_generator',
+              ['boolnet/bintools/example_generator.pyx'],
               include_dirs=include_dirs),
-
-    Extension("boolnet.exptools.fastrand",
-              ["boolnet/exptools/fastrand.pyx"],
+    Extension('boolnet.exptools.fastrand',
+              ['boolnet/exptools/fastrand.pyx'],
               libraries=cython_gsl.get_libraries(),
               library_dirs=[cython_gsl.get_library_dir()],
               include_dirs=include_dirs),
-
-    Extension("boolnet.network.algorithms",
-              ["boolnet/network/algorithms.pyx"],
+    # C++
+    Extension('boolnet.network.algorithms',
+              ['boolnet/network/algorithms.pyx'],
               language='c++',
               include_dirs=include_dirs),
-    Extension("boolnet.network.boolnet",
-              ["boolnet/network/boolnet.pyx"],
+    Extension('boolnet.network.boolnet',
+              ['boolnet/network/boolnet.pyx'],
               language='c++',
               include_dirs=include_dirs),
-
-    Extension("boolnet.learning.networkstate",
-              ["boolnet/learning/networkstate.pyx"],
+    Extension('boolnet.learning.networkstate',
+              ['boolnet/learning/networkstate.pyx'],
               language='c++',
               include_dirs=include_dirs),
     ]
