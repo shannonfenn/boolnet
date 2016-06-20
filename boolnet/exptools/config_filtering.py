6a79b80b0d7bfb2d3c4c49b09926f5b55fda506b
Shannon Fenn <shannon.fenn@gmail.com>
2016-06-20 11:51:06 +1000

    Bug fix from improper prior change
    config_filtering was not updated to handle list style log_keys

--- boolnet/exptools/config_filtering.py
+++ boolnet/exptools/config_filtering.py
@@ -2,11 +2,11 @@ import re
 from collections import MutableMapping
 
 
-def paths_with_values(item, path=[]):
+def path_value_pairs(item, path=[]):
     ''' recursively traverses dictionary and yields all full paths. '''
     if isinstance(item, MutableMapping):
         for key, subitem in item.items():
-            for child in paths_with_values(subitem, path + [key]):
+            for child in path_value_pairs(subitem, path + [key]):
                 yield child
     else:
         yield path, item
@@ -22,12 +22,11 @@ def list_regex_match(pattern, path):
 
 
 def filter_keys(source, path_map):
-    ''' this updates a dict with another where the two may contain nested
-        dicts themselves (or more generally nested mutable mappings). '''
+    ''' this filters a dict (potentially containing sub-dicts)
+        using a list of key/path pairs. '''
     sink = {}
-    for path, val in paths_with_values(source):
-        for sink_key, pattern in path_map.items():
-            # print(sink_key, path, pattern, list_regex_match(path, pattern))
+    for path, val in path_value_pairs(source):
+        for sink_key, pattern in path_map:
             if list_regex_match(pattern, path):
                 # regex value insertion
                 if '{}' in sink_key:
