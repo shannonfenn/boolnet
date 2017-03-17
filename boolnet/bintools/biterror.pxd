4026ced1cebebfed4b168e0a26bb73b0ddc2888f
Shannon Fenn <shannon.fenn@gmail.com>
2017-03-17 13:41:50 +1100

    [tests] new unit test for e2 weight vector

--- boolnet/bintools/biterror.pxd
+++ boolnet/bintools/biterror.pxd
@@ -49,7 +49,7 @@ cdef class StandardE1(Evaluator):
 
 
 cdef class StandardE2(Evaluator):
-    cdef double[:] weight_vector
+    cdef readonly double[:] weight_vector
 
     cpdef double evaluate(self, packed_type_t[:, ::1] E, packed_type_t[:, ::1] T)
 
--- boolnet/test/test_biterror.py
+++ boolnet/test/test_biterror.py
@@ -1,6 +1,6 @@
 from boolnet.bintools.functions import function_from_name
 import numpy as np
-from boolnet.bintools.biterror import EVALUATORS
+from boolnet.bintools.biterror import EVALUATORS, StandardE2
 
 
 def test_function_value(error_matrix_harness):
@@ -24,6 +24,14 @@ def test_function_value(error_matrix_harness):
         actual = error_evaluator.evaluate(E, T)
         np.testing.assert_array_almost_equal(actual, expected)
 
+
+def test_e2_weight_vector(error_matrix_harness):
+    order = np.array([4, 3, 8, 0, 2, 7, 1, 5, 6], dtype=np.uintp)
+    expected = np.array([5, 6, 1, 9, 7, 2, 8, 4, 3], dtype=float)
+    expected /= expected.sum()
+    evaluator = StandardE2(1, len(order), order)
+    actual = evaluator.weight_vector
+    np.testing.assert_array_almost_equal(actual, expected)
 
 # @pytest.mark.python
 # def test_error(error_matrix_harness, function):
