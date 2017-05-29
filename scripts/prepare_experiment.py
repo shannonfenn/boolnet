4efad8045f6cc6f23ad6c3281cfa906c9f734c79
Shannon Fenn <shannon.fenn@gmail.com>
2017-05-29 16:04:58 +1000

    [feature] divided pickled configurations
     - prepare_experiment.py prepares parameters for each configuration in a separate pickle file in <resultdir>/working
     - run_single_experiment.py runs a single such experiment and saves the result map to a matching json file in the same directory

---
+++ scripts/prepare_experiment.py
@@ -0,0 +1,97 @@
+from datetime import datetime       # for date for result dir
+import os                           # for mkdir
+import os.path                      # for path manipulation
+import yaml                         # for loading experiment files
+try:
+    from yaml import CSafeLoader as Loader
+except ImportError:
+    from yaml import SafeLoader as Loader
+import shutil                       # file copying
+import argparse                     # CLI
+import itertools                    # imap and count
+import pickle
+
+import boolnet.exptools.config_tools as cfg
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('experiment',
+                        type=argparse.FileType('r'),
+                        help='experiment config filename.')
+    parser.add_argument('-d', '--directory', type=str, metavar='dir',
+                        default='HMRI/experiments/results',
+                        help='working directory to dump configurations in.')
+    parser.add_argument('-b', '--batch-mode', action='store_true',
+                        help='suppress progress bars.')
+    return parser.parse_args()
+
+
+def create_result_dir(base_dir, exp_name):
+    ''' Generates a new timestamped directory for the results and copies
+        the experiment script, experiment file and git hash into it.'''
+    # make directory for the results
+    working_dir = os.path.join(base_dir, '{}_{}_'.format(
+        exp_name, datetime.now().strftime('%y-%m-%d')))
+
+    # find the first number i such that 'Results_datetime_i' is not
+    # already a directory and make that new directory
+    working_dir += next(str(i) for i in itertools.count()
+                        if not os.path.isdir(working_dir + str(i)))
+    os.makedirs(working_dir)
+
+    # return the directory name for the results to go in
+    return working_dir
+
+
+def initialise(args):
+    ''' This is responsible for setting up the output directory.'''
+    # load experiment file
+    settings = yaml.load(args.experiment, Loader=Loader)
+
+    # create result directory
+    working_dir = create_result_dir(args.directory, settings['name'])
+
+    # copy experiment config into results directory
+    shutil.copy(args.experiment.name, working_dir)
+
+    return settings, working_dir
+
+
+def dump_tasks(tasks, working_dir):
+    for i, task in enumerate(tasks):
+        with open('{}/working/{}.exp'.format(working_dir, i), 'wb') as f:
+            pickle.dump(task, f)
+
+
+# ############################## MAIN ####################################### #
+def main():
+    args = parse_arguments()
+
+    settings, working_dir = initialise(args)
+
+    print('Directories initialised.')
+    print('Prepping experiment files in: ' + working_dir + '\n')
+
+    # generate learning tasks
+    try:
+        configurations = cfg.generate_configurations(settings,
+                                                     args.batch_mode)
+        print('Done: {} configurations generated.'.format(
+            len(configurations)))
+        tasks = cfg.generate_tasks(configurations, args.batch_mode)
+        print('Done: {} tasks generated.\n'.format(len(tasks)))
+
+        os.makedirs(os.path.join(working_dir, 'working'))
+
+        dump_tasks(tasks, working_dir)
+    except cfg.ValidationError as err:
+        print(err)
+        print('\nFailed to prep experiment.')
+        print('Failed! Directory will still exist: {}'.format(working_dir))
+        return
+
+    print('Success! Experiment prepped in: {}.'.format(working_dir))
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/run_single_experiment.py
@@ -0,0 +1,35 @@
+import pickle
+import argparse
+import json
+from boolnet.exptools.learn_boolnet import learn_bool_net
+from boolnet.exptools.config_tools import ExperimentJSONEncoder
+
+
+def main(directory, index):
+    expfile = '{}/working/{}.exp'.format(directory, index)
+    resultfile = '{}/working/{}.json'.format(directory, index)
+
+    with open(expfile, 'rb') as f:
+        task = pickle.load(f)
+
+    result = learn_bool_net(task)
+
+    with open(resultfile, 'w') as stream:
+        json.dump(result, stream, cls=ExperimentJSONEncoder)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('exp_directory', type=str,
+                        help='experiment result directory.')
+    parser.add_argument('index', type=int,
+                        help='experiment index to run.')
+    args = parser.parse_args()
+    main(args.exp_directory, args.index)
+
+# combine script
+
+# write "["
+# cat first subfile
+# cat each subsequent subfile prepended with "," and ended with "\n"
+# write "]"
