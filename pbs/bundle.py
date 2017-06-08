55a3afef108f19f538d50f35a1b2e5c801d7de13
Shannon Fenn <shannon.fenn@gmail.com>
2017-06-08 12:22:21 +1000

    [feature] bundling script now dumps bundle list to file

--- pbs/bundle.py
+++ pbs/bundle.py
@@ -1,6 +1,6 @@
 import argparse
 import glob
-from os.path import isdir, expanduser, abspath, splitext
+from os.path import isdir, expanduser, abspath, splitext, join
 from natsort import natsorted
 
 
@@ -17,23 +17,27 @@ def parse_args():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('dir', type=directory_type)
     parser.add_argument('--num', '-n', type=int)
-    parser.add_argument('--file', '-f', type=argparse.FileType())
+    parser.add_argument('--infile', '-i', type=argparse.FileType(),
+        help='optional file listing experiments (.exp) to run')
+    parser.add_argument('--outfile', '-o', type=argparse.FileType('w'),
+        help=('output file to dump list of bundles (.explist)'
+              ', default: <dir>/bundles'))
 
     args = parser.parse_args()
 
-    if len(args.jobname) > 10:
-        parser.error('jobname ({}) must be 10 char or less.'.format(
-            args.jobname))
     if not args.num:
         args.num = 7500
     elif not (0 < args.num <= 7500):
         parser.error('--num must be in [1..7500].')
 
+    if not args.outfile:
+        args.outfile = open(join(args.dir, 'bundles'), 'w')
+
     return args
 
 
 def get_experiments_from_file(stream):
-    experiments = stream.read().splitlines()
+    experiments = stream.read().splitlines(keepends=True)
     stream.close()
     return experiments
 
@@ -43,7 +47,7 @@ def get_remaining_experiments(directory):
     json_iter = glob.iglob('{}/working/*.json'.format(directory))
     all_exp = set(splitext(f)[0] for f in exp_iter)
     all_json = set(splitext(f)[0] for f in json_iter)
-    return natsorted(f + '.exp' for f in all_exp - all_json)
+    return natsorted(f + '.exp\n' for f in all_exp - all_json)
 
 
 def strided(l, n):
@@ -56,16 +60,20 @@ def strided(l, n):
 def main():
     args = parse_args()
 
-    if args.file:
-        experiments = get_experiments_from_file(args.file)
+    if args.infile:
+        experiments = get_experiments_from_file(args.infile)
     else:
         experiments = get_remaining_experiments(args.dir)
 
     bundles = strided(experiments, args.num)
 
     for i, bundle in enumerate(bundles):
-        with open('{}/working/{}.explist'.format(args.dir, i)) as f:
+        fname = join(args.dir, 'working', '{}.explist'.format(i))
+        with open(fname, 'w') as f:
             f.writelines(bundle)
+        args.outfile.write(fname + '\n')
+
+    args.outfile.close()
 
 
 if __name__ == '__main__':
