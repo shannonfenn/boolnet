4e761407b2fb8f628efe6752af48d86c22af0e47
Shannon Fenn <shannon.fenn@gmail.com>
2019-01-20 15:38:52 +0900

    more flexible testing
    test_learn_boolnet now allows specifying runs to use. Should simplify debugging.

--- boolnet/test/test_learn_boolnet.py
+++ boolnet/test/test_learn_boolnet.py
@@ -11,11 +11,25 @@ from boolnet.exptools import learn_boolnet
 
 def task_expectation_pairs():
     for folder in glob('boolnet/test/runs/*/'):
+        print(folder)
         with open(f'{folder}/config.yaml') as f:
             top_config = yaml.load(f)
         tasks, _ = config_tools.generate_tasks(top_config, True)
         with open(f'{folder}/expected.json') as f:
             for i, tsk, exp in zip(count(), tasks, f):
+                tsk['id'] = i
+                exp = json.loads(exp)
+                strip_times(exp)
+                yield tsk, exp
+
+
+def specific_harnesses(name, ids):
+    with open(f'boolnet/test/runs/{name}/config.yaml') as f:
+        top_config = yaml.load(f)
+    tasks, _ = config_tools.generate_tasks(top_config, True)
+    with open(f'boolnet/test/runs/{name}/expected.json') as f:
+        for i, tsk, exp in zip(count(), tasks, f):
+            if i in ids:
                 tsk['id'] = i
                 exp = json.loads(exp)
                 strip_times(exp)
@@ -28,15 +42,17 @@ def strip_times(record):
         record.pop(k)
 
 
+# @pytest.mark.parametrize('task,expected', specific_harnesses('gen10', [1, 3, 12]))
 @pytest.mark.parametrize('task,expected', task_expectation_pairs())
 def test_run(task, expected):
-    print(task['id'], task['learner']['name'])
+    print(task['id'], task['learner']['name'], task['notes_tmt'])
     result = learn_boolnet.learn_bool_net(task)
     strip_times(result)
     result['id'] = task['id']
     # pass through json dump/load to deal with annoying floating point issues
     actual = json.loads(json.dumps(result, cls=NumpyAwareJSONEncoder,
                         separators=(',', ':')))
+
     # TEMPORARY
     if 'feature_sets' in actual and 'feature_sets' not in expected:
         actual.pop('feature_sets')
@@ -48,5 +64,4 @@ def test_run(task, expected):
     print(expected)
     print()
     print(actual)
-    # assert False
     assert actual == expected
